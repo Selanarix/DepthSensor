1f59d57ccc0e8bd67c0a4047cdedd810f26f1ff4
Jonathan Schubert <js@twocoders.de>
2014-01-10 14:09:44 +0000

    fix syntax bugs, and get it running in first simple version

--- webFrontend/input.php
+++ webFrontend/input.php
@@ -1,32 +1,32 @@
 <?php
 if(!(isset($_GET["sensor"]) && isset($_GET["value"])))
-	exit();
+	exit("No val");
 
 $sensor = $_GET["sensor"];
 $value = $_GET["value"]; 
 
-if(!(is_numeric($sensor) && is_numberic($value)))
+if(!(is_numeric($sensor) && is_numeric($value)))
 	exit();
 if($value < 0 || $value > 100000)
 	exit();
 if($sensor < 0 || $sensor >6)
 	exit();
 
-if(!is_file("sensorData".$sensor.".txt")
+if(!is_file("sensorData".$sensor.".txt"))
 {
 	$fp=fopen("sensorData".$sensor.".txt","w") or die();
 	fclose($fp);
 }
 $lines = file("sensorData".$sensor.".txt");
-array_push($lines,$value);
+array_push($lines,$value."\n");
 
 $fp=fopen("sensorData".$sensor.".txt","w") or die();
 $sizeOfArray=count($lines);
 for($i = 0; $i < $sizeOfArray; $i++)
 {
-	if(($sizeOfArray - $i) > 100)
+	if(($sizeOfArray - $i) > 10)
 		continue;
-	fwrite($fp,lines[$i]."\r\n");
+	fwrite($fp,$lines[$i]);
 }
 fclose($fp);
 ?>
