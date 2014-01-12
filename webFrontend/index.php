4e203dac067b97d6ed5bbd84d1aa723c127b7502
Jonathan Schubert <js@twocoders.de>
2014-01-12 12:44:49 +0000

    index.php parses xml now. Also fix some spelling errors in code and xml

--- webFrontend/index.php
+++ webFrontend/index.php
@@ -1,6 +1,40 @@
 <?php
+
+function parseSensorXML($filename)
+{
+    $dataSet = array();
+    
+    if(empty($filename) || !file_exists($filename))
+        return false;
+
+    $sensor = simplexml_load_file($filename);
+    $configTag = $sensor->configuration;
+
+    //parse config part
+    $dataSet["floorArea"] = (int)$configTag->floorArea;
+    $dataSet["tankHeight"] = (int)$configTag->TankHeight;
+    $dataSet["colorGraph"] = (string)$configTag->ColorOfGraph;
+    $dataSet["name"] = (string)$configTag->NameForID;
+    $dataSet["description"] = (string)$configTag->TankDescription;
+    $dataSet["sizeOfHistory"] = (int)$configTag->SizeOfHistory;
+    $dataSet["id"] = (int)$sensor["sensorID"];
+    $dataSet["depthMeasurements"] = array();
+
+        /* For each <character> node, we echo a separate <name>. */
+    foreach ($sensor->measurements->measurement as $measure) 
+    {
+       $measureSet = array();
+       $measureSet["value"] = (int)$measure;
+       $measureSet["time"] = (int)$measure["measurementTime"];
+       array_push($dataSet["depthMeasurements"],$measureSet);
+    }
+
+    return $dataSet;
+}
+
+
 $dataFiles = array();
-$sensorData = array();
+$sensorDataSets = array();
 
 $handle = opendir('.');
 if ($handle === false) 
@@ -10,11 +44,13 @@ while (false !== ($entry = readdir($handle)))
 {
     if(strpos($entry,'sensorData') !== false)
 	{
-       echo $entry."<br>";
-        $dataSet = array();
-	    $dataSet = file($entry);
-	    array_push($sensorData,$dataSet);
+        $sensorData = array();
+        $sensorData = parseSensorXML($entry);
+        if($sensorData === false)
+           echo "could not read xml file";
+        else
+           array_push($sensorDataSets,$sensorData);
     }
 }	
-print_r($sensorData);
+print_r($sensorDataSets);
 ?>
