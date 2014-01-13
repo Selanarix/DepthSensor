be6a59bd2c745267b3853a7243b9831c51ba8df0
Max <hempe@hm.edu>
2014-01-13 00:34:09 +0000

    cleared php errors in index.php missing: label of x-axis

--- webFrontend/index.php
+++ webFrontend/index.php
@@ -88,22 +88,25 @@ function graphDefineData($sensorDataSets)
 		}
 		//print_r($values);
 		//print_r($time);
-		$nameOfVal = "Values".$i;
+		$nameOfVal = "Serie".$i;
+		$nameOfX = "XLabel".$i;
 		
 		$DataSet->AddPoint($values,$nameOfVal);
-		$DataSet->AddPoint($time,"Timestamp");
-		$DataSet->SetSerieName("Timestamp","Time");
-		$DataSet->SetSerieName($nameOfVal,"Depth");
+		$DataSet->AddPoint($time,$nameOfX);
+		$DataSet->SetAbsciseLabelSerie($nameOfX);
+		$DataSet->RemoveSerie($nameOfX);   
+		$DataSet->AddAllSeries();
+		
+		$DataSet->SetSerieName("Time".$i,$nameOfX);
+		$DataSet->SetSerieName("Depth".$i,$nameOfVal);
 		
 		//Set Timestemp as X-Axis and Value to Y-Axis
-		$DataSet->SetAbsciseLabelSerie("Timestamp");
 		$DataSet->setXAxisName("Time");
-		$DataSet->setXAxisFormat("H:i");
+		$DataSet->setXAxisFormat("time");
 		
-		$DataSet->setYAxisName(0, "Depth");
-		$DataSet->setYAxisUnit(0, "cm");
+		$DataSet->setYAxisName("Depth");
+		$DataSet->setYAxisUnit("cm");
 		
-		$DataSet->AddAllSeries();
         $i++;
 	}
 	
@@ -113,30 +116,32 @@ function graphDefineData($sensorDataSets)
 function graphInitGraph($DataSet)
 {
 	// Initialise the graph
-	 $Test = new pChart(700,230);
-	 $Test->setFixedScale(0,50);
-	 $Test->setFontProperties("graph/Fonts/tahoma.ttf",8);
-	 $Test->setGraphArea(50,30,585,200);
-	 $Test->drawFilledRoundedRectangle(7,7,693,223,5,240,240,240);
-	 $Test->drawRoundedRectangle(5,5,695,225,5,230,230,230);
-	 $Test->drawGraphArea(255,255,255,TRUE);
-	 $Test->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,TRUE,0,2);
-	 $Test->drawGrid(4,TRUE,230,230,230,50);
+	 $Graph = new pChart(700,230);
+	 $Graph->setFixedScale(0,50);
+	 $Graph->setFontProperties("graph/Fonts/tahoma.ttf",8);
+	 $Graph->setGraphArea(50,30,585,200);
+	 $Graph->drawFilledRoundedRectangle(7,7,693,223,5,240,240,240);
+	 $Graph->drawRoundedRectangle(5,5,695,225,5,230,230,230);
+	 $Graph->drawGraphArea(255,255,255,TRUE);
+	 $Graph->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,TRUE,0,2);
+	 $Graph->drawGrid(4,TRUE,230,230,230,50);
 	
-	return $Test;
+	// Draw the 0 line   
+	$Graph->setFontProperties("Fonts/tahoma.ttf",6);   
+	$Graph->drawTreshold(0,143,55,72,TRUE,TRUE);   
+	return $Graph;
 }
 
-function graphRender($DataSet, $Test)
+function graphRender($DataSet, $Graph)
 {
-	$Test->drawLineGraph($DataSet->GetData(),$DataSet->GetDataDescription());  
-	$Test->drawPlotGraph($DataSet->GetData(),$DataSet->GetDataDescription(),3,2,255,255,255); 
-	
+	// Draw the cubic curve graph  
+	$Graph->drawCubicCurve($DataSet->GetData(),$DataSet->GetDataDescription());
 	// Finish the graph  
-	$Test->setFontProperties("graph/Fonts/tahoma.ttf",8);  
-	//$Test->drawLegend(45,35,$DataSet->GetDataDescription(),255,255,255);  
-	$Test->setFontProperties("graph/Fonts/tahoma.ttf",10);  
-	$Test->drawTitle(60,22,"Amount of water during time",50,50,50,585);  
-	$Test->Render("Naked.png"); 
+	$Graph->setFontProperties("graph/Fonts/tahoma.ttf",8);  
+	//$Graph->drawLegend(45,35,$DataSet->GetDataDescription(),255,255,255);  
+	$Graph->setFontProperties("graph/Fonts/tahoma.ttf",10);  
+	$Graph->drawTitle(60,22,"Amount of water during time",50,50,50,585);  
+	$Graph->Render("Naked.png"); 
 }
 ?>
 
