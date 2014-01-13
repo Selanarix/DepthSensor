a9cf8adb2979d25a4aad3a0bd9d0a7f07b65b40e
Max <hempe@hm.edu>
2014-01-13 19:45:07 +0000

    -design for webFrontend with bootstrap

--- webFrontend/statistics.php
+++ webFrontend/statistics.php
@@ -10,7 +10,10 @@ include("helper/parseSensorData.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Depth Seonsor Project">
     <meta name="author" content="Maximilian Hempe und Jonathan Schubert">
-
+	
+	<script src="javascript/jquery.js"></script>
+	<script src="javascript/imgAutoReload.js" type="text/javascript"></script>
+	
     <title>Depth Sensor Project Page</title>
 
     <!-- Bootstrap core CSS -->
@@ -34,14 +37,14 @@ include("helper/parseSensorData.php");
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Depth Sensor</a>
+              <a class="navbar-brand" href="/">Depth Sensor</a>
             </div>
             <div class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="">Home</a></li>
-                <li><a href="statistics.php">Statistics</a></li>
+                <li><a href="/">Home</a></li>
+                <li class="active"><a href="statistics.php">Statistics</a></li>
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sensor Data<b class="caret"></b></a>
+                  <a href="" class="dropdown-toggle" data-toggle="dropdown">Sensor Data<b class="caret"></b></a>
                   <ul class="dropdown-menu">
                     <li><a href="sensor.php?sensor=0">Sensor0</a></li>
                     <li><a href="#">Sensor1</a></li>
@@ -72,14 +75,8 @@ include("helper/parseSensorData.php");
           <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide">
           <div class="container">
             <div class="carousel-caption">
-				<h1>Depth Sensor Project</h1>
-              
-				<?php
-				foreach($_SESSION['data'] as $sensorData)
-				{
-					echo '<img height="100px" id="sensor'.$sensorData["id"].'" src="dynPicProcessing/tankImgCreate.php?sensor='.$sensorData["id"].'" />'."\r\n";
-				}
-				?>
+			  <h1>Level Statistics</h1>
+              <p>This Project gets data of <?php echo count($_SESSION['data'])?> sensor(s) in your watertanks. On this page you find statistics about chronological sequence of water level.</p>
             </div>
           </div>
         </div>
@@ -94,33 +91,23 @@ include("helper/parseSensorData.php");
 
     <div class="container marketing">
 
-      <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="col-lg-3">
-          <img class="img-circle" data-src="holder.js/140x140" alt="Tank0">
-          <h2>Heading</h2>
-          <p>This graphic shows the actual level of the first Tank.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-3">
-          <img class="img-circle" data-src="holder.js/140x140" alt="Tank1">
-          <h2>Heading</h2>
-          <p>This graphic shows the actual level of the second Tank.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-3">
-          <img class="img-circle" data-src="holder.js/140x140" alt="Tank2">
-          <h2>Heading</h2>
-          <p>This graphic shows the actual level of the third Tank.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-      </div><!-- /.row -->
+      <!-- two columns of text below the carousel -->
+      
+			<?php
+				foreach($_SESSION['data'] as $sensorData)
+				{
+					echo '<div class="row">';
+					echo '<img class="autoreloadImg" id="sensor'.$sensorData["id"].'" src="dynPicProcessing/renderGraph.php?sensor='.$sensorData["id"].'" />'."\r\n";
+					echo '</div> <!--close row -->';
 
-      <footer>
-		<br>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2014 Jonathan Schubert & Maximilian Hempe &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-      </footer>
+				}
+			?>
+	
+		<footer>
+			<br>
+			<p class="pull-right"><a href="#">Back to top</a></p>
+			<p>&copy; 2014 Jonathan Schubert & Maximilian Hempe &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+		</footer>
 
     </div><!-- /.container -->
 
