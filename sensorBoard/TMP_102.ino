c3650466e1a694c52b0dffb7ecf861d24d7adcb9
Jonathan Schubert <js@twocoders.de>
2014-02-01 01:28:07 +0000

    start writing driver for TMP102 temperature sensor board using I2C

--- sensorBoard/TMP_102.ino
+++ sensorBoard/TMP_102.ino
@@ -25,6 +25,10 @@ namespace TMP_102
         T_High = 0x02,
     } PointerAddress;
 
+    const unsigned int SDA_Pin = A4;
+    const unsigned int SCL_Pin = A5;
+    const double DegreePerTick = 0.0625;
+
     //Methods
     static I2C_Addresses getAddr(const TMP102* const thi);
     static const TermostatLimits* getLimit(const TMP102* const thi);
@@ -34,7 +38,9 @@ namespace TMP_102
     static void termostatEnable(const TMP102* const thi, double min, double max, TermostatMode mod, ConsecutiveFaults faul, AlertPinPolarity pol);
 
     static bool initI2C(const TMP102* const thi);
-
+    static inline void setPointerReg(const TMP102* const thi, PointerAddress pAddr)
+    static inline uint16_t getConfig(const TMP102* const thi);
+    static inline void setConfig(const TMP102* const thi,uint16_t conf);
     
 
     bool construct(TMP102* thi,I2C_Addresses add)
@@ -50,10 +56,10 @@ namespace TMP_102
 
         thi->readTemperatur = simpleRead;
         thi->oneShotRead = oneRead;
-        thi->setToLowPowerMode = toLowPower;
+        thi->setToLowPowerMode = toLowPower;//D
         thi->setTermostatMode = termostatEnable
-        thi->getTermostatLimits = getLimit; 
-        thi->getAddress = getAddr;
+        thi->getTermostatLimits = getLimit; //D
+        thi->getAddress = getAddr;//D
 
         return initI
     }
@@ -70,12 +76,24 @@ namespace TMP_102
 
     static double simpleRead(const TMP102* const thi)
     {
-        
+        setPointerReg(thi, Temperatur);
+        Wire.endTransmission();
+
+        Wire.requestFrom(thi->addr, 2);
+
+        uint16_t msb = Wire.read();
+        uint16_t lsb = Wire.read();
+
+        int value = ((msb<<8 | lsb)) >> 4;
+
+        return (double)value * DegreePerTick;
     }
 
     static void toLowPower(const TMP102* const thi)
     {
-    
+        uint16_t conf = getConfig(thi);
+        conf |= (1<<8);
+        setConfig(thi, conf);
     }
 
     static void termostatEnable(const TMP102* const thi, double min, double max, TermostatMode mod, ConsecutiveFaults faul, AlertPinPolarity pol)
@@ -85,8 +103,33 @@ namespace TMP_102
 
     static void initI2C(const TMP102* const thi)
     {
-        
+        pinMode(SDA_Pin, INPUT_PULLUP);
+        pinMode(SCL_Pin, INPUT_PULLUP);
     }
-    
 
+    static inline void setPointerReg(const TMP102* const thi, PointerAddress pAddr)
+    {
+        Wire.beginTransmission(thi->addr); 
+        Wire.write(pAddr);
+    }
+
+    static inline uint16_t getConfig(const TMP102* const thi)
+    {
+        uint16_t res = 0;
+        setPointerReg(thi, Config);
+        Wire.endTransmission();
+        Wire.requestFrom(thi->addr, 2);
+        res = Wire.read();
+        res = (res<<8) | Wire.read();
+        return res;
+    }
+
+    static inline void setConfig(const TMP102* const thi,uint16_t conf)
+    {
+        uint16_t res = 0;
+        setPointerReg(thi, Config);
+        Wire.write((uint8_t)(conf>>8));
+        Wire.write((uint8_t)conf);
+        Wire.endTransmission();
+    }
 }
